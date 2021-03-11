9f1f01d7c1893c42ff86af2a410e5b965863d149
Jonathan-Majors <izzymajors@gmail.com>
2021-03-11 16:15:30 -0600

    wip2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+require('dotenv').config()
+
 console.log("deploy app")
+
+const express = require('express')
+
+
+const server = express()
+
+server.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+const cors = require('cors')
+server.use(cors())
+}
+
